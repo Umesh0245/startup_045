2ed67c38790b82933b7bcf10862890e05f7d265c
vamshmvr <vamsh@speshway.com>
2026-04-01 01:47:29 +0530

    added client folder

---
+++ client/src/config/api.ts
@@ -0,0 +1,7 @@
+export const API = {
+  AUTH: "http://localhost:3001/api/v1",
+  VENDOR: "http://localhost:3002/api/v1",
+  REQUIREMENT: "http://localhost:3003/api/v1",
+  RFQ: "http://localhost:3004/api/v1",
+  AI: "http://localhost:3009/api/v1"
+};
